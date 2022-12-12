c3c0c43c5add7a3c42fc9b691bf4a60a0e2bac63
lizguillen27 <lizguillen27@gmail.com>
2022-12-12 13:26:27 -0500

    list all accounts of the service

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,20 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["get"])
+def list_accounts():
+    """
+    Lists of Accounts
+    This endpoint will list all accounts
+    """
+    app.logger.info("Request to list accounts")
+    
+    accounts= Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
+
 
 
 ######################################################################
@@ -79,13 +92,6 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-
-
-
-
-    
-    
-
 
 
 ######################################################################
